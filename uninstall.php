27f4e49b7251cc2c552afa8cc0be9e5d96b0c9e8
Ronald Drenth <ronald@vdlp.nl>
2018-07-10 19:49:33 +0200

    Implement uninstalling the plugin

--- uninstall.php
+++ uninstall.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Fired when the plugin is uninstalled.
  *
@@ -13,3 +12,31 @@
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
+
+/**
+ * Setup composer autoloading
+ */
+require_once __DIR__ . '/vendor/autoload.php';
+
+/**
+ * Delete options used by this plugin.
+ */
+delete_option( 'hydro_raindrop_application_id' );
+delete_option( 'hydro_raindrop_client_id' );
+delete_option( 'hydro_raindrop_client_secret' );
+delete_option( 'hydro_raindrop_environment' );
+
+/**
+ * Delete user metadata by for this plugin.
+ */
+delete_metadata( 'user', 0, 'hydro_id', '', true );
+delete_metadata( 'user', 0, 'hydro_mfa_enabled', '', true );
+delete_metadata( 'user', 0, 'hydro_raindrop_confirmed', '', true );
+
+/**
+ * Delete access token from the transient token storage.
+ */
+require_once __DIR__ . '/includes/class-hydro-raindrop-token-storage.php';
+
+$storage = new Hydro_Raindrop_TransientTokenStorage();
+$storage->unsetAccessToken();
